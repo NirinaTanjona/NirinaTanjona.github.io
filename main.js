faac71feff660c4b211ed87b599998ea5cd4d113
NirinaTanjona <tanjonawork@gmail.com>
2021-07-18 19:15:21 +0300

    header fixed

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-document.getElementsByClassName('navbar-toggler')[0].addEventListener("click", toggle);
-
+document.getElementsByClassName("navbar-toggler")[0].addEventListener("click", toggle);
+let x = document.getElementsByClassName("navbar-container")[0];
+let collapse = document.getElementsByClassName("navbar-collapse")[0];
+let nav = x.innerHTML;
+collapse.innerHTML = nav;
 
 function toggle() {
   let isClick = document.getElementsByClassName("is-clicked")[0]; 
   let button = document.getElementsByClassName("navbar-toggler")[0]; 
   if (isClick === undefined) {
     button.classList.add("is-clicked");
+    collapse.style.display = "block";
+    document.getElementsByClassName("header-nav-main")[1].classList.add("collapse");
   } else {
     button.classList.remove("is-clicked");
+    collapse.style.display = "none";
+    document.getElementsByClassName("header-nav-main")[1].classList.remove("collapse");
   }
 }
